59f41e3a4e7318d128cc32b2c1c8ef756f734c56
Johnny <johnny@jober.io>
2018-03-02 16:51:55 +0900

    react strap - bootstrap v4 applied

--- src/App.js
+++ src/App.js
@@ -27,15 +27,13 @@ class App extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
+          <h1>맨날 개발만 하고... 여친한테 선물 마지막으로 한 게 언제니?</h1>
           <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/stuff">Stuff</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink exact to="/">홈</NavLink></li>
+            <li><NavLink to="/contact">정체가 뭐냐</NavLink></li>
           </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
-            <Route path="/stuff" component={Stuff}/>
             <Route path="/contact" component={Contact}/>
           </div>
         </div>
--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,9 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        <h2>GOT QUESTIONS?</h2>
-        <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
+        <h2>우리는</h2>
+        <p>
+          오늘도 디버깅과 신규 기능 개발에 야근을 밥먹듯이 하고 있는 당신들의 여자친구를 위해 무엇을 도울 수 있는지 고민하는 사람들
         </p>
       </div>
     );
