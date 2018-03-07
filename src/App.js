d986503e105a68a48426712b36dcc5b0a25a5c12
Minhoko <minoerick120@gmail.com>
2018-03-08 00:41:29 +0700

    add: setting eslint

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
 import {
   Route,
   NavLink,
   HashRouter
-} from "react-router-dom";
-import Home from "./Home";
-import Contact from "./Contact";
-import "./index.css";
+} from 'react-router-dom';
 
+// Utilities
+import ReactGA from 'react-ga';
+
+// Component
+import { Home, Contact } from './component';
+
+// Style
+import './index.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      someData: null,
+      someData: null
     };
+  }
 
+  componentWillMount() {
     // Add your tracking ID created from https://analytics.google.com/analytics/web/#home/
     ReactGA.initialize('UA-114772027-01');
     // This just needs to be called once since we have no routes in this case.
     ReactGA.pageview(window.location.pathname);
   }
+
   render() {
     return (
       <HashRouter>
@@ -32,14 +39,14 @@ class App extends Component {
             <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/contact" component={Contact}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
           </div>
           <div>
-          <h2>쿠스랩</h2>
-          <h3>대표: 구일모</h3>
-          <h3>사업자등록번호: 606-466-9713</h3>
-          <h3>사업장 소재지: 서울특별시 동작구 사당로2길 76 1101호</h3>
+            <h2>쿠스랩</h2>
+            <h3>대표: 구일모</h3>
+            <h3>사업자등록번호: 606-466-9713</h3>
+            <h3>사업장 소재지: 서울특별시 동작구 사당로2길 76 1101호</h3>
           </div>
         </div>
       </HashRouter>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
@@ -10,7 +10,7 @@ class Home extends Component {
         <h2>주말엔 곧 벚꽃 구경도 간다는데...</h2>
         <h2>우리 회사는 제품 런칭이 4월이라고 하네요...</h2>
         <h2>삐진 여친님을 위해 선물 하나 여친님 직장으로 보내보시죠</h2>
-        <img src={require('./img/flower.jpg')} />
+        <img alt="flower" src={require('../img/flower.jpg')} />
         <h2>싱싱하고 풍성한 도매시장에서 직접 구한 생화 가득</h2>
         <h2>원하시는 시간, 원하시는 장소로 배송해 드립니다.</h2>
       </div>
